5a9a3631ca6f96550d0f8db084709e802aaac84f
NANO1903 <fernando.beristain.alvarez@gmail.com>
2025-11-27 09:39:27 -0600

    fix: CORS Policy

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     cors: {
       origin: [
+        "http://http://132.196.250.98",
         "http://127.0.0.1:3000",
-        "http://192.168.0.*:30000",
       ],
-      credentials: true,
     }
   });
   const port = process.env.PORT || 3000;
